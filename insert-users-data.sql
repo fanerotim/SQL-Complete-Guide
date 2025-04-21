2abdc59128f04a2cf977016204b5d172390d699f
Petar Cholakov <petar.cholakov30@gmail.com>
2025-04-21 17:49:56 +0100

    add more entries to 'users' table

---
+++ insert-users-data.sql
@@ -0,0 +1,27 @@
+-- Inserting values into postgresql database
+-- INSERT INTO users (yearly_salary, full_name, current_status) VALUES (
+--     40000,
+--     'Petar Cholakov',
+--     'self-employed'
+-- );
+
+-- Inserting data into MySQL database - status column name differs to the one in postgresql (current_status)
+
+-- INSERT INTO users (yearly_salary, full_name, status) VALUES (
+--     40000,
+--     'Petar Cholakov',
+--     'employed'
+-- );
+
+-- Add more users to practice
+-- INSERT INTO users (yearly_salary, full_name, status) VALUES (
+--     0,
+--     'Mihail Ivanov',
+--     'unemployed'
+-- );
+
+INSERT INTO users (yearly_salary, full_name, status) VALUES (
+    50000,
+    'Georgi Topchev',
+    'self-employed'
+);
