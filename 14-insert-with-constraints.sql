7a0e8598b48dbe70be2117c751a73b4d476b11bc
Petar Cholakov <petar.cholakov30@gmail.com>
2025-04-22 20:24:59 +0100

    add examples of inserting values into not-nullable table columns

--- 14-insert-with-constraints.sql
+++ 14-insert-with-constraints.sql
@@ -1,5 +1,14 @@
-INSERT INTO users (full_name, current_status) 
-VALUES (
-    'Paisii Petkanov',
-    'self-employed'
+-- Example of inserting data into table columns that have NOT-NULL constraint (PostgreSQL)
+
+-- INSERT INTO users (full_name, current_status) 
+-- VALUES (
+--     'Paisii Petkanov',
+--     'self-employed'
+-- );
+
+-- Example of inserting values into table columns that are not-nullable (MySQL)
+INSERT INTO conversations (user_name, employer_name) 
+VALUES(
+    'PTZ',
+    'Petko Petkov Zlatanov'
 );
