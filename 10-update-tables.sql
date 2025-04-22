07f5f2edbc1745a8cb0299d39f9ce75f5c34a946
Petar Cholakov <petar.cholakov30@gmail.com>
2025-04-22 14:34:32 +0100

    add more examples on how to update existing table columns in both MySQL and PostgreSQL

---
+++ 10-update-tables.sql
@@ -0,0 +1,15 @@
+-- Example of table update / modification that works on MySQL
+-- ALTER TABLE employers
+-- MODIFY COLUMN yearly_revenue NUMERIC(5, 2);
+
+-- Example of the same command as above, but for PostgreSQL
+-- ALTER TABLE employers
+-- ALTER COLUMN yearly_revenue SET DATA TYPE NUMERIC(5, 2);
+
+-- Another example that works on PostgreSQL
+-- ALTER TABLE employers
+-- ALTER COLUMN company_address SET DATA TYPE VARCHAR(300);
+
+-- Same example as the one above, but this works in MySQL
+ALTER TABLE employers
+MODIFY COLUMN company_address VARCHAR(300)
