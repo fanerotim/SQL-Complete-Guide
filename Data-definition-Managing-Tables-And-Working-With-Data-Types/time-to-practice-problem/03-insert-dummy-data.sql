1ba772a6ac4ebe2d8cedb343ca1e4fdbe06dda76
Petar Cholakov <petar.cholakov30@gmail.com>
2025-04-25 19:52:19 +0100

    add postgre example

--- Data-definition-Managing-Tables-And-Working-With-Data-Types/time-to-practice-problem/03-insert-dummy-data.sql
+++ Data-definition-Managing-Tables-And-Working-With-Data-Types/time-to-practice-problem/03-insert-dummy-data.sql
@@ -1,18 +1,18 @@
--- INSERT INTO products(product_name, price, description, amount_in_stock, image)
--- VALUES(
---     'Nike T-Shirt',
---     30,
---     'Ideal for running in hot weather',
---     30,
---     'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5d9b28b6-ded5-4c72-92d4-679637a90ac1/M+NK+DFADV+STRIDE+SS+TOP.png'
--- );
+INSERT INTO products(product_name, price, description, amount_in_stock, image)
+VALUES(
+    'Nike T-Shirt',
+    30,
+    'Ideal for running in hot weather',
+    30,
+    'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5d9b28b6-ded5-4c72-92d4-679637a90ac1/M+NK+DFADV+STRIDE+SS+TOP.png'
+);
 
 -- Test constrainsts of tables
 
-INSERT INTO products (price, description, amount_in_stock, image)
-VALUES (
-    100,
-    'Perfect for hot weather',
-    2,
-    'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1718987641-ciele-shorts-6675ab33b4516.jpg?crop=1xw:1xh;center,top&resize=980:*'
-);
+-- INSERT INTO products (price, description, amount_in_stock, image)
+-- VALUES (
+--     100,
+--     'Perfect for hot weather',
+--     2,
+--     'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1718987641-ciele-shorts-6675ab33b4516.jpg?crop=1xw:1xh;center,top&resize=980:*'
+-- );
--- Data-definition-Managing-Tables-And-Working-With-Data-Types/time-to-practice-problem/04-update-table-add-constraints.sql
+++ Data-definition-Managing-Tables-And-Working-With-Data-Types/time-to-practice-problem/04-update-table-add-constraints.sql
@@ -1,6 +1,17 @@
+-- MySQL
+-- ALTER TABLE products
+-- MODIFY COLUMN product_name VARCHAR(300) NOT NULL,
+-- MODIFY COLUMN price NUMERIC(10, 2) CHECK (price > 0) NOT NULL,
+-- MODIFY COLUMN description TEXT NOT NULL,
+-- MODIFY COLUMN amount_in_stock INT NOT NULL CHECK (amount_in_stock > 0),
+-- MODIFY COLUMN image VARCHAR(300) NOT NULL;
+
+-- PostgreSQL
 ALTER TABLE products
-MODIFY COLUMN product_name VARCHAR(300) NOT NULL,
-MODIFY COLUMN price NUMERIC(10, 2) CHECK (price > 0) NOT NULL,
-MODIFY COLUMN description TEXT NOT NULL,
-MODIFY COLUMN amount_in_stock INT NOT NULL CHECK (amount_in_stock > 0),
-MODIFY COLUMN image VARCHAR(300) NOT NULL;
+ALTER COLUMN product_name SET NOT NULL,
+ALTER COLUMN price SET NOT NULL,
+ALTER COLUMN description SET NOT NULL,
+ALTER COLUMN amount_in_stock SET NOT NULL,
+ALTER COLUMN image SET NOT NULL,
+ADD CONSTRAINT price_positive CHECK (price > 0),
+ADD CONSTRAINT amount_in_stock_positive CHECK (amount_in_stock >= 0);
