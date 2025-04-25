f8c82f386382899cdd892595ca0fff07973af1c9
Petar Cholakov <petar.cholakov30@gmail.com>
2025-04-25 16:10:20 +0100

    add more examples of primary key usage

--- 16-primary-key.sql
+++ 16-primary-key.sql
@@ -1,18 +1,36 @@
--- Example of a new table that has an id column; MySQL example;
+-- CREATE TYPE employment_status AS ENUM('emplyed', 'unemployed', 'self-employed'); -- PostgreSQL
 
--- CREATE TABLE books (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     author VARCHAR(250) NOT NULL,
---     title VARCHAR(250) NOT NULL,
---     summary VARCHAR(500)
+-- CREATE TABLE users (
+--     id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--     -- id SERIAL PRIMARY KEY, -- PostgreSQL
+--     full_name VARCHAR(300) NOT NULL,
+--     yearly_salary INT,
+--     current_status ENUM('employed', 'self-employed', 'unemployed') -- MySQL 
+--     -- current_status employment_status -- PostgreSQL
 -- );
 
--- Same example as above, but for PostgreSQL
+-- CREATE TABLE employers (
+--     id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--     -- id SERIAL PRIMARY KEY, -- PostgreSQL
+--     company_name VARCHAR(250) NOT NULL,
+--     company_address VARCHAR(300) NOT NULL,
+--     yearly_revenue NUMERIC(5, 2),
+--     is_hiring BOOLEAN NOT NULL
+-- );
 
-CREATE TABLE books (
-    id SERIAL PRIMARY KEY,
-    author VARCHAR(250) NOT NULL,
-    title VARCHAR(250) NOT NULL,
-    summary VARCHAR(500)
+-- CREATE TABLE conversations (
+--     -- id SERIAL PRIMARY KEY, -- PostgreSQL
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     user_id INT,
+--     employer_id INT,
+--     message TEXT NOT NULL,
+--     date_sent TIMESTAMP
+-- );
+
+INSERT INTO conversations (user_id, employer_id, message)
+VALUES(
+    1,
+    2,
+    'Hi, I`m interested in your company!'
 );
 
