abe834c6649bb20dc3313a6386845c72977e1074
Petar Cholakov <petar.cholakov30@gmail.com>
2025-04-29 20:34:07 +0100

    add a new example of multiple INNER JOINs

--- 04-inner-join.sql
+++ 04-inner-join.sql
@@ -3,7 +3,13 @@
 -- INNER JOIN addresses AS addr ON usr.id = addr.city_id;
 
 -- Example with multiple INNER JOINs
-SELECT usr.first_name, usr.last_name, addr.street, addr.house_number, city.name AS city_name
+-- SELECT usr.first_name, usr.last_name, addr.street, addr.house_number, city.name AS city_name
+-- FROM users AS usr
+-- INNER JOIN addresses AS addr ON addr.id = usr.address_id
+-- INNER JOIN cities AS city ON city.id = addr.city_id
+-- WHERE city.name = 'Stara Zagora';
+
+SELECT city.name as city_name, addr.street, addr.house_number
 FROM users AS usr
-INNER JOIN addresses AS addr ON addr.id = usr.address_id
-INNER JOIN cities AS city ON city.id = addr.city_id
+INNER JOIN cities AS city ON usr.address_id = city.id
+INNER JOIN addresses AS addr ON city.id = addr.id;
