71f06b4481f7362cb33a8ce73526121eac57dc50
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-01 07:25:16 +0100

    add example of multiple LEFT JOINs

--- Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/06-left-join.sql
+++ Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/06-left-join.sql
@@ -1,3 +1,8 @@
+-- SELECT *
+-- FROM cities AS city
+-- LEFT JOIN addresses AS addr ON city.id = addr.city_id;
+
 SELECT *
-FROM cities AS city
-LEFT JOIN addresses AS addr ON city.id = addr.city_id;
+FROM users AS usr
+LEFT JOIN addresses AS addr ON usr.id = addr.id
+LEFT JOIN cities AS cty ON addr.id = cty.id;
