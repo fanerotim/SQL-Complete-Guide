2f63abfe93a3533f1a803f129589e2cdf762cfbc
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-02 20:19:29 +0100

    add one more JOIN example

--- Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/08-more-joins.sql
+++ Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/08-more-joins.sql
@@ -1,4 +1,9 @@
-SELECT usr.email, addr.street, addr.house_number, cty.name
-FROM users AS usr
-INNER JOIN addresses AS addr ON usr.address_id = addr.id
-INNER JOIN cities AS cty ON addr.city_id = cty.id;
+-- SELECT usr.email, addr.street, addr.house_number, cty.name
+-- FROM users AS usr
+-- INNER JOIN addresses AS addr ON usr.address_id = addr.id
+-- INNER JOIN cities AS cty ON addr.city_id = cty.id;
+
+SELECT cty.name AS city_name, addr.street, addr.house_number
+FROM cities AS cty
+-- LEFT JOIN addresses AS addr ON addr.city_id = cty.id; -- LEFT JOIN returns all cities in db even if they do not have an address
+INNER JOIN addresses AS addr ON addr.city_id = cty.id; -- INNER JOIN returns only cities with an address
