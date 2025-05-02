f9d69d1109513123ebbaf3f8578ad2b61c40ba54
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-02 05:14:10 +0100

    add more dummy data

---
+++ Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/07-insert-more-data.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO cities (name)
+-- VALUES 
+--     ('London'), 
+--     ('New York'), 
+--     ('Amsterdam'), 
+--     ('Berlin');
+
+INSERT INTO addresses (street, house_number, city_id)
+VALUES
+    ('Random street', '11B', 5),
+    ('Random street', '1A', 2),
+    ('Pancake street', '5', 1),
+    ('Plum street', '23', 3);
