f96235d6cae7cada64b2bdd6454e055f26a4361e
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-03 20:26:06 +0100

    add more example of cascading on delete

--- Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/13-delete-rows-with-references.sql
+++ Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/13-delete-rows-with-references.sql
@@ -1,5 +1,10 @@
 -- DELETE FROM users
 -- WHERE id = 1;
 
-DELETE FROM addresses
+-- Deleting an address automatically deletes the user of that address
+-- DELETE FROM addresses
+-- WHERE id = 2;
+
+-- Deleting a city automatically deletes all addresses linked to that city
+DELETE FROM cities 
 WHERE id = 2;
