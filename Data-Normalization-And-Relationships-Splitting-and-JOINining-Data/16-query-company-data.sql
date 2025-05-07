5fea3a1ce70537a26ba1cab5aaa1dadb3d3051dc
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-07 04:58:11 +0100

    add another example that uses intermediate table

---
+++ Data-Normalization-And-Relationships-Splitting-and-JOINining-Data/16-query-company-data.sql
@@ -0,0 +1,16 @@
+-- SELECT e.first_name, e.last_name, t.team_name 
+-- FROM employees AS e
+-- INNER JOIN teams AS t ON t.id = e.team_id
+-- WHERE t.id = 1;
+
+-- SELECT e.first_name, e.last_name, cb.name
+-- FROM employees AS e
+-- INNER JOIN company_buildings AS cb ON cb.id = e.id
+-- WHERE cb.id = 1;
+
+SELECT p.id AS project_id, e.first_name, e.last_name, p.description
+FROM employees_projects AS ep
+INNER JOIN employees AS e ON ep.employee_id = e.id
+INNER JOIN projects AS p ON ep.project_id = p.id
+WHERE e.id = 3
+ORDER BY e.id;
