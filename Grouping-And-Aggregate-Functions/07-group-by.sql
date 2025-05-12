db99be80ba5fc849b8413fb1d6a53e31cce4235e
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-12 05:30:10 +0100

    add another example of GROUP BY with joins and two aggregate functions

--- Grouping-And-Aggregate-Functions/07-group-by.sql
+++ Grouping-And-Aggregate-Functions/07-group-by.sql
@@ -1,3 +1,8 @@
-SELECT booking_date, SUM(amount_tipped)
-FROM bookings
-GROUP BY booking_date;
+-- SELECT booking_date, SUM(amount_tipped)
+-- FROM bookings
+-- GROUP BY booking_date;
+
+SELECT b.booking_date, SUM(b.amount_billed), ROUND(AVG(b.amount_tipped), 2) AS avg_amount_tipped, pm.name AS payment_method
+FROM bookings AS b
+INNER JOIN payment_methods AS pm ON b.payment_id = pm.id
+GROUP BY b.booking_date, pm.name;
