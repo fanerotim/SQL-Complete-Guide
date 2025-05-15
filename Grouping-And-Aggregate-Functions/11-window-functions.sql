5275cbca7f0b88d1a76e48a39471b30446c8c862
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-15 05:15:04 +0100

    add another example of window function with RANK

--- Grouping-And-Aggregate-Functions/11-window-functions.sql
+++ Grouping-And-Aggregate-Functions/11-window-functions.sql
@@ -5,11 +5,16 @@
 -- OVER(PARTITION BY booking_date)
 -- FROM bookings;
 
-SELECT 
-    b.booking_date, 
-    SUM(b.guests) OVER(PARTITION BY b.guests) AS total_guests
-FROM bookings AS b
-ORDER BY total_guests DESC;
+-- SELECT 
+--     b.booking_date, 
+--     SUM(b.guests) OVER(PARTITION BY b.guests) AS total_guests
+-- FROM bookings AS b
+-- ORDER BY total_guests DESC;
 
+SELECT 
+    b.booking_date,
+    amount_tipped,
+    RANK() OVER(PARTITION BY b.amount_billed ORDER BY b.amount_tipped DESC)
+FROM bookings AS b;
 
 
