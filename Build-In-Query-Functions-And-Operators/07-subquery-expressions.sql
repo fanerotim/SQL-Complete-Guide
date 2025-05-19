78c57ead9335c764dcc61e88a018690fbb52409b
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-19 18:25:07 +0100

    add one more EXISTS example

--- Build-In-Query-Functions-And-Operators/07-subquery-expressions.sql
+++ Build-In-Query-Functions-And-Operators/07-subquery-expressions.sql
@@ -6,10 +6,20 @@
 -- );
 
 -- EXAMPLE OF EXISTS() function
-SELECT c.first_name, c.last_name, c.email
-FROM customers AS c
-WHERE EXISTS (
-    SELECT o.amount_billed
-    FROM orders AS o
-    WHERE o.customer_id = c.id AND c.id = 3
-);
+-- SELECT c.first_name, c.last_name, c.email
+-- FROM customers AS c
+-- WHERE EXISTS (
+--     SELECT o.amount_billed
+--     FROM orders AS o
+--     WHERE o.customer_id = c.id AND c.id = 3
+-- );
+
+-- ANOTHER EXAMPLE
+SELECT SUM(o.amount_billed) AS total_amount_paid
+-- SELECT o.amount_billed
+FROM orders AS o
+WHERE EXISTS(
+    SELECT c.id
+    FROM customers AS c
+    WHERE c.id = o.customer_id AND c.email = 'dimov.g@gmail.com'
+); 
