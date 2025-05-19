59270207b4e917d99c540e87b0290011b5d9f39b
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-19 20:27:47 +0100

    clarify notes

--- Build-In-Query-Functions-And-Operators/10-subquery-expressions-IN.sql
+++ Build-In-Query-Functions-And-Operators/10-subquery-expressions-IN.sql
@@ -1,18 +1,19 @@
 -- Different queries to return the email of customers that have placed an order
 
+-- This returns all customers even those that did not place an order
 -- SELECT email FROM customers;
 
--- JOIN returns duplicate emails (not just unique values)
+-- JOIN returns only customers that placed an order, but it returns duplicate values (emails in this case), not just unique values
 -- SELECT c.email
 -- FROM customers AS c
 -- INNER JOIN orders AS o ON c.id = customer_id;
 
 -- Example of IN() - this query returs the emails of people with names defined in the IN function
-SELECT c.email
-FROM customers AS c
-WHERE c.first_name IN('Petko', 'Goshe')
+-- SELECT c.email
+-- FROM customers AS c
+-- WHERE c.first_name IN('Petko', 'Goshe')
 
--- Returns unique emails of people that have placed an order
+-- Returns unique values (emails in this case) of customers that have placed an order. no duplicate values.
 -- SELECT c.email
 -- FROM customers AS c
 -- WHERE c.id IN (
