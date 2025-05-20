d4c0e2d7c98e0ae41c03274105522bee0ac3a039
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-20 20:20:28 +0100

    edit task description

--- Build-In-Query-Functions-And-Operators/11-module-challenge.sql
+++ Build-In-Query-Functions-And-Operators/11-module-challenge.sql
@@ -1,4 +1,4 @@
--- Task is to query last_checkin colmun from memberships table and output what day it was when the user last checked in
+-- Task is to check the last_checkin table and output what day of the week (Mon-Fri) it was on that date
 
 -- VERY IMPERFECT SOUTION, BUT IT WORKS
 -- SELECT last_checkin, WEEKDAY(last_checkin) + 1,
@@ -11,22 +11,22 @@
 --         WHEN WEEKDAY(last_checkin) + 1 = 6 THEN 'Saturday'
 --         WHEN WEEKDAY(last_checkin) + 1 = 7 THEN 'Sunday'
 --     END AS cur_day
--- FROM memberships AS m;
+-- FROM memberships;
 
 
-SELECT cur_day, last_checkin,
-    CASE 
-        WHEN cur_day = 1 THEN 'Monday'
-        WHEN cur_day = 2 THEN 'Tuesday'
-        WHEN cur_day = 3 THEN 'Wednesday'
-        WHEN cur_day = 4 THEN 'Thursday'
-        WHEN cur_day = 5 THEN 'Friday'
-        WHEN cur_day = 6 THEN 'Saturday'
-        WHEN cur_day = 7 THEN 'Sunday'
-    END AS day_of_week
-FROM (
-    SELECT WEEKDAY(last_checkin) + 1 AS cur_day, last_checkin
-    FROM memberships
-) AS cur_dates_table;
+-- SELECT cur_day, last_checkin,
+--     CASE 
+--         WHEN cur_day = 1 THEN 'Monday'
+--         WHEN cur_day = 2 THEN 'Tuesday'
+--         WHEN cur_day = 3 THEN 'Wednesday'
+--         WHEN cur_day = 4 THEN 'Thursday'
+--         WHEN cur_day = 5 THEN 'Friday'
+--         WHEN cur_day = 6 THEN 'Saturday'
+--         WHEN cur_day = 7 THEN 'Sunday'
+--     END AS day_of_week
+-- FROM (
+--     SELECT WEEKDAY(last_checkin) + 1 AS cur_day, last_checkin
+--     FROM memberships
+-- ) AS cur_dates_table;
 
 
