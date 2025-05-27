7d9e3e66dab7d17a16788943fbdc55c74f110c1c
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-27 20:22:34 +0100

    insert dummy data

---
+++ Summary-And-Practice-Complete-Example/03-inserting-data.sql
@@ -0,0 +1,16 @@
+INSERT INTO events(
+    name,
+    image,
+    date_time,
+    description,
+    max_participants,
+    min_age
+)
+VALUES
+    ('Event 1', 'image1.jpg', '2025-05-27 20:20', 'desc 1', 20, 7),
+    ('Event 2', 'image2.jpg', '2025-05-27 20:20', 'desc 2', 20, 7),
+    ('Event 3', 'image3.jpg', '2025-05-27 20:20', 'desc 3', 20, 7),
+    ('Event 4', 'image4.jpg', '2025-05-27 20:20', 'desc 4', 20, 7),
+    ('Event 5', 'image5.jpg', '2025-05-27 20:20', 'desc 5', 20, 7),
+    ('Event 6', 'image6.jpg', '2025-05-27 20:20', 'desc 6', 20, 7);
+
