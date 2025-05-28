f5bc7910d6063eed7e58f4cd05f12527fb3e1bb4
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-28 09:52:12 +0100

    modify project tables

--- Summary-And-Practice-Complete-Example/02-create-project-tables.sql
+++ Summary-And-Practice-Complete-Example/02-create-project-tables.sql
@@ -1,21 +1,21 @@
--- CREATE TABLE users(
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     -- id SERIAL PRIMARY KEY, -- PostgreSQL
---     first_name VARCHAR(250) NOT NULL,
---     last_name VARCHAR(250) NOT NULL,
---     birthdate DATE NOT NULL,
---     email VARCHAR(250) UNIQUE NOT NULL
--- );
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL,
+    birthdate DATE NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL
+);
 
--- CREATE TABLE organizers(
---     id INT PRIMARY KEY AUTO_INCREMENT, 
---     -- id SERIAL PRIMARY KEY, -- PostgreSQL,
---     password VARCHAR(250) NOT NULL,
---     user_id INT NOT NULL,
---     event_id INT NOT NULL,
---     FOREIGN KEY (user_id) REFERENCES users (id),
---     FOREIGN KEY (event_id) REFERENCES events (id)
--- );
+CREATE TABLE organizers(
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL,
+    password VARCHAR(250) NOT NULL,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id)
+);
 
 CREATE TABLE events(
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -26,48 +26,50 @@ CREATE TABLE events(
     description TEXT NOT NULL,
     max_participants INT CHECK (max_participants > 0),
     min_age INT CHECK (min_age > 0)
-    -- organizer_id INT NOT NULL,
-    -- tag_id INT NOT NULL,
-    -- location_id INT NOT NULL,
-    -- FOREIGN KEY (organizer_id) REFERENCES organizers (id),
-    -- FOREIGN KEY (tag_id) REFERENCES tags (id),
-    -- FOREIGN KEY (location_id) REFERENCES locations (id)
+    organizer_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    location_id INT NOT NULL,
+    FOREIGN KEY (organizer_id) REFERENCES organizers (id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tags (id),
+    FOREIGN KEY (location_id) REFERENCES locations (id) ON DELETE CASCADE
 );
 
--- CREATE TABLE locations(
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     -- id SERIAL PRIMARY KEY, -- PostgreSQL
---     street VARCHAR(250) NOT NULL, 
---     house_number VARCHAR(250) NOT NULL,
---     postal_code VARCHAR(250) NOT NULL,
---     city_id INT NOT NULL,
---     FOREIGN KEY (city_id) REFERENCES cities (id)
--- );
+CREATE TABLE locations(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    street VARCHAR(250) NOT NULL, 
+    house_number VARCHAR(250) NOT NULL,
+    postal_code VARCHAR(250) NOT NULL,
+    city_id INT NOT NULL,
+    FOREIGN KEY (city_id) REFERENCES cities (id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
 
--- CREATE TABLE cities(
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     -- id SERIAL PRIMARY KEY, -- PostgreSQL
---     name VARCHAR(250) NOT NULL
--- );
+CREATE TABLE cities(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    name VARCHAR(250) NOT NULL
+);
 
--- CREATE TABLE tags(
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     -- id SERIAL PRIMARY KEY,  -- PostgreSQL
---     name VARCHAR(250) UNIQUE NOT NULL
--- );
+CREATE TABLE tags(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    -- id SERIAL PRIMARY KEY,  -- PostgreSQL
+    name VARCHAR(250) UNIQUE NOT NULL
+);
 
--- CREATE TABLE tags_events(
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     -- id SERIAL PRIMARY KEY, -- PostgreSQL
---     tag_id INT NOT NULL,
---     event_id INT NOT NULL
--- );
+CREATE TABLE tags_events(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    tag_id INT NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
+);
 
--- CREATE TABLE users_events(
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     -- id SERIAL PRIMARY KEY, -- PostgreSQL
---     user_id INT NOT NULL,
---     event_id INT NOT NULL,
---     FOREIGN KEY (user_id) REFERENCES users (id),
---     FOREIGN KEY (event_id) REFERENCES events (id)
--- );
+CREATE TABLE users_events(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
+);
---
+++ Summary-And-Practice-Complete-Example/04-modify-data.sql
@@ -0,0 +1,7 @@
+-- UPDATE events
+-- SET min_age = 10
+-- WHERE name = 'Event 1' OR name = 'Event 3'
+
+-- SELECT *
+-- FROM events
+-- ORDER BY id DESC;
